7fcdc3e6a33530706e89d0ee614ff9c317fb75d9
periclescesar <periclescesar@gmail.com>
2022-10-23 02:19:07 -0300

    feat(list): create endpoint to get all task

--- src/application/Router.ts
+++ src/application/Router.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import TasksController from '@app/controller/TasksController'
-import { CreateTaskUseCase } from '@domain/tasks'
+import { CreateTaskUseCase, ListTasksUseCase } from '@domain/tasks'
 import KnexTaskRepository from '@infra/repositories/knexTaskRepository'
 import { DatabaseClient } from '@infra/clients'
 import knex from 'knex'
@@ -9,6 +9,13 @@ import logger from '@app/logger'
 import { EventBus } from '@infra/events'
 
 const Router = express.Router()
+
+const dbClient = new DatabaseClient(knex(dbConfig), logger)
+const taskRepo = new KnexTaskRepository(dbClient)
+const eventBus = new EventBus()
+const createTaskUC = new CreateTaskUseCase(taskRepo, eventBus)
+const listTasksUC = new ListTasksUseCase(taskRepo)
+const controller = new TasksController(createTaskUC, listTasksUC)
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 Router.get('/health', async (_req, res) => {
@@ -38,13 +45,29 @@ Router.post('/tasks', async (req, res) => {
    #swagger.responses[200] = {
      description: 'task successfully created.',
    }
+   #swagger.responses[404] = {
+     description: 'has an error on task definition.',
+   }
    */
-  const dbClient = new DatabaseClient(knex(dbConfig), logger)
-  const taskRepo = new KnexTaskRepository(dbClient)
-  const eventBus = new EventBus()
-  const createTaskUC = new CreateTaskUseCase(taskRepo, eventBus)
-  const controller = new TasksController(createTaskUC)
-  await controller.createTasks(req, res)
+
+  await controller.createTask(req, res)
 })
 
+Router.get('/tasks', async (req, res) => {
+  /**
+   #swagger.summary = 'to save a task performed.'
+   #swagger.parameters['X-Request-Id'] = {
+     in: 'header',
+     description: 'to identify request on log'
+   }
+   #swagger.responses[200] = {
+     description: 'task successfully created.',
+   }
+   #swagger.responses[404] = {
+     description: 'has an error on task definition.',
+   }
+   */
+
+  await controller.listTasks(req, res)
+})
 export default Router
--- src/application/controller/TasksController.ts
+++ src/application/controller/TasksController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express'
 import HttpError from '@app/HttpError'
-import Task, { CreateTaskUseCase } from '@domain/tasks'
+import Task, { CreateTaskUseCase, ListTasksUseCase } from '@domain/tasks'
 
 export default class TasksController {
   constructor (
     private readonly createTaskUseCase: CreateTaskUseCase,
+    private readonly listTaskUseCase: ListTasksUseCase,
   ) {}
 
-  async createTasks (req: Request, res: Response): Promise<void> {
+  async createTask (req: Request, res: Response): Promise<void> {
     let task: Task
     try {
       task = new Task(req.body.summary, req.body.user.name)
@@ -20,6 +21,12 @@ export default class TasksController {
 
     task = await this.createTaskUseCase.handle(task)
     res.json(task)
+    res.status(200)
+  }
+
+  async listTasks (_req: Request, res: Response): Promise<void> {
+    const tasks = await this.listTaskUseCase.handle()
+    res.json(tasks)
     res.status(200)
   }
 }

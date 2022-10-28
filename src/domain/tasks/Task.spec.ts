841dc098477ff9d253e98323c1744416531552da
periclescesar <periclescesar@gmail.com>
2022-10-27 22:22:29 -0300

    test(task): improve task tests

--- src/domain/tasks/Task.spec.ts
+++ src/domain/tasks/Task.spec.ts
@@ -5,13 +5,23 @@ import { DomainError } from '@domain'
 describe('Task', () => {
   it('fail: summary more than 2500', async () => {
 
-    expect(() => {new Task(faker.lorem.paragraphs(100), faker.name.fullName(), undefined, raw.id)})
+    expect(() => {new Task(faker.lorem.paragraphs(100), faker.name.fullName())})
       .toThrowError(new DomainError('the summary is more than 2500 characters'))
   })
 
   it('success: new task', async () => {
-
-    expect(() => {new Task(faker.lorem.paragraphs(1), faker.name.fullName(), undefined, raw.id)})
+    let task: Task
+    expect(() => {task = new Task(faker.lorem.paragraphs(1), faker.name.fullName())})
       .not.toThrowError(DomainError)
+
+    expect(task!.id).toBeUndefined()
+  })
+
+  it('success: new task to update', async () => {
+    let task: Task
+    expect(() => {task = new Task(faker.lorem.paragraphs(1), faker.name.fullName(), undefined, 1)})
+      .not.toThrowError(DomainError)
+
+    expect(task!.id).toBe(1)
   })
 })

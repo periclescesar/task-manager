c0764ba18f3d6ebb1f269a88be844c27f9bc682e
periclescesar <periclescesar@gmail.com>
2022-10-29 12:50:02 -0300

    feat(auth): create authorize middleware

--- src/application/middleware/authorize.ts
+++ src/application/middleware/authorize.ts
@@ -1,16 +1,20 @@
-import { Request, Response } from 'express'
+import { Response } from 'express'
 import { NextFunction } from 'express-serve-static-core'
 import { Role } from '@domain/users'
+import { expressjwt as jwt, Request as JWTRequest } from 'express-jwt'
+import jwtConfig from '@config/jwtConfig'
 
 const authorize = (roles: Role[] = []) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    const userRole = req.get('role') ?? ''
+  return [
+    jwt({ secret: jwtConfig.secret, algorithms: ["HS256"] }),
+    (req: JWTRequest, res: Response, next: NextFunction): void => {
+    const userRole = req.auth?.role
     if (!roles.includes(<Role>userRole)) {
       res.status(401).json({ message: 'unauthorized' })
       return
     }
     next()
-  }
+  }]
 }
 
 export default authorize

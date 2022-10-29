4df675c8a6ae9107fa0a7a4c4e8f764e4a94cefe
periclescesar <periclescesar@gmail.com>
2022-10-29 18:29:08 -0300

    fix(task): fix max limit constrains

--- migrations/20221023013856_tasks.ts
+++ migrations/20221023013856_tasks.ts
@@ -6,7 +6,7 @@ import Task from '@domain/tasks'
 export async function up (knex: Knex): Promise<void> {
   await knex.schema.createTable(KnexTaskRepository.tableName, (table: CreateTableBuilder) => {
     table.increments('id')
-    table.text('summary').notNullable().checkLength('<', Task.SUMMARY_MAX_LENGTH)
+    table.text('summary').notNullable().checkLength('<=', Task.SUMMARY_MAX_LENGTH)
     table.string('user').notNullable()
   })
 }
